7505613cee22478f6ac492feaeb483123794adff
Chaitanya Yadav <cayadav2009@gmail.com>
2016-03-25 22:46:50 +0530

    Simple Form Installed and Application file updated

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -3,24 +3,39 @@ class PinsController < ApplicationController
 	before_action :get_pin, only:[:show,:edit,:update,:destroy]
 
 	def index
+		@pins = Pin.all.order("created_at DESC")
 	end
 
 	def show
 	end
 
 	def new
+		@pin = Pin.new
 	end
 
 	def create
+		@pin = Pin.new(pins_params)
+		if @pin.save
+			redirect_to @pins_params
+		else
+			render 'new'
+		end
 	end
 
 	def edit
 	end
 
 	def update
+		if @pin.update(pins_params)
+			redirect_to @pins_params
+		else 
+			render 'edit'
+		end
 	end
 
 	def destroy
+		@pin.destroy
+		redirect_to root_path
 	end
 
 	private
